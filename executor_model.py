e992804db7d54d92b048eb4539037d8a3909314d
lilwilde16 <lilwilde16@gmail.com>
2025-05-05 23:59:06 -0500

    Add files via upload

---
+++ executor_model.py
@@ -0,0 +1,8 @@
+
+def suggest_trades(data_file):
+    return {
+        "symbol": "NAS100",
+        "direction": "buy",
+        "confidence": 0.74,
+        "reason": "Strength rising + trend positive"
+    }
